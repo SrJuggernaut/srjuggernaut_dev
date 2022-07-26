039cd20f2c453a068df0df808435df04a3dc99e0
SrJuggernaut <jugger@srjuggernaut.dev>
2022-07-25 19:38:52 -0500

    feat: fontawesome support

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,9 @@
 import AppContextProvider from '@contexts/app/AppContextProvider'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 import type { AppProps } from 'next/app'
+
+config.autoAddCss = false
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
