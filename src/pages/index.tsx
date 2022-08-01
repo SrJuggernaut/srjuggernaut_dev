385c0f2c6e80d38afefba9fc7f3790964fa1d762
SrJuggernaut <jugger@srjuggernaut.dev>
2022-08-01 15:03:09 -0500

    feat: static home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,108 @@
+import { Box, Button, Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import NextLink from 'next/link'
+
+import Contained from '@components/layouts/Contained'
+import Imagotype from '@components/logo/Imagotype'
+import Logo from '@components/logo/Logo'
+import ScreenReaderOnly from '@styles/screenReaderOnly'
+import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <>
-      <h1>Next Js</h1>
-    </>
+    <Contained>
+      <Box
+        sx={(theme) => ({
+          display: 'grid',
+          alignItems: 'center',
+          gridTemplateColumns: '1fr',
+          minHeight: '70vh',
+          [theme.breakpoints.up('md')]: {
+            gridTemplateColumns: '1fr 1fr'
+          }
+        })}
+      >
+        <div>
+          <Imagotype
+            sx={{
+              width: '100%',
+              height: 'auto'
+            }}
+          />
+        </div>
+        <div>
+          <Logo sx={{
+            width: '100%',
+            height: 'auto'
+          }} />
+          <ScreenReaderOnly>SrJuggernaut</ScreenReaderOnly>
+          <Typography variant="h2" align="center">Diseño web imparable</Typography>
+        </div>
+      </Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          minHeight: '70vh'
+        }}
+      >
+        <Typography variant="h2" align="center">Curriculum</Typography>
+        <Box
+          sx={(theme) => ({
+            display: 'grid',
+            gridTemplateColumns: '1fr',
+            alignItems: 'center',
+            gap: 1,
+            [theme.breakpoints.up('sm')]: {
+              gridTemplateColumns: '1fr 1fr'
+            }
+          })}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              '&>div': {
+                width: '75%',
+                height: 'auto'
+              }
+            }}
+          >
+            <div>
+              <Image
+                src="/img/Juan_Carlos_Sanchez.png"
+                width={600}
+                height={600}
+              />
+            </div>
+          </Box>
+          <div>
+            <Typography gutterBottom variant="h3" align="center">Juan Carlos Sánchez Méndez</Typography>
+            <Typography variant="body1" align="center">Soy un FullStack developer Javascript. Quiero crear sitios web y sistemas que impacten positivamente a negocios y comunidades, ganándome la vida con lo que me gusta hacer.</Typography>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                padding: 1
+              }}
+            >
+              <NextLink
+                href="/curriculum"
+                passHref
+              >
+                <Button
+                  variant="contained"
+                >
+                  Ver Curriculum
+                </Button>
+              </NextLink>
+            </Box>
+          </div>
+        </Box>
+      </Box>
+    </Contained>
   )
 }
 
