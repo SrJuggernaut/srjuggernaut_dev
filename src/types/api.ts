101c3f4582281ca0f9ba8665c8fbc26d0404c1b6
SrJuggernaut <jugger@srjuggernaut.dev>
2024-02-20 17:27:51 -0600

    chore: remove unused ApiError class

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,15 +1,3 @@
-// TODO: La clase ApiError no es un tipo, debería moverla a un archivo aparte
-export class ApiError extends Error {
-  status: number
-  message: string
-  constructor (status: number, message: string) {
-    super(message)
-    this.status = status
-    this.message = message
-    Object.setPrototypeOf(this, ApiError.prototype)
-  }
-}
-
 export interface ApiResponse<T> {
   message: string
   data?: T
