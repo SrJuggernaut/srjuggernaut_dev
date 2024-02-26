c9d35b292c44b5e48ad5dbdda8a684eb232abdce
SrJuggernaut <jugger@srjuggernaut.dev>
2024-02-26 11:00:50 -0600

    fix: env variables APP_NAME & PORT

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'srjuggernaut_dev',
+      name: process.env.APP_NAME,
       script: 'bun',
       args: 'run start',
       env: {
-        NODE_ENV: 'development',
-        PORT: process.env.PORT,
-        NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
-        NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
-        APPWRITE_KEY_SECRET: process.env.APPWRITE_KEY_SECRET,
-        ADMIN_EMAIL: process.env.ADMIN_EMAIL
-      },
-      env_production: {
         NODE_ENV: 'production',
         PORT: process.env.PORT,
         NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
@@ -31,6 +23,8 @@ module.exports = {
       path: process.env.DEPLOY_PATH,
       'post-deploy': 'pm2 --silent startOrRestart ecosystem.config.js',
       env: {
+        name: process.env.APP_NAME,
+        PORT: process.env.PORT,
         NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
         NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
         APPWRITE_KEY_SECRET: process.env.APPWRITE_KEY_SECRET,
@@ -45,6 +39,7 @@ module.exports = {
       path: process.env.DEPLOY_PATH,
       'post-deploy': 'pm2 --silent startOrRestart ecosystem.config.js',
       env: {
+        name: process.env.APP_NAME,
         PORT: process.env.PORT,
         NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
         NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
