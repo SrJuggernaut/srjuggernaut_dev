1ea627b5f90b08f1b0ad4a07b25c9f6313019e74
SrJuggernaut <jugger@srjuggernaut.dev>
2024-02-26 12:07:47 -0600

    fix: github actions workflows (#2)
    * feat: move lifecycle to prestart script
    
    * fix: env variables APP_NAME & PORT
    
    * fix: prepare script not run husky on production
    
    * fix: github workflows trigger conditions
    
    * fix: env passed to workflow
    
    * fix: app name env variable
    
    * feat: sharp for image optimization
    
    * chore: updated dependencies

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,10 @@
 module.exports = {
   apps: [
     {
-      name: 'srjuggernaut_dev',
+      name: process.env.APP_NAME,
       script: 'bun',
       args: 'run start',
       env: {
-        NODE_ENV: 'development',
-        PORT: process.env.PORT,
-        NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
-        NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
-        APPWRITE_KEY_SECRET: process.env.APPWRITE_KEY_SECRET,
-        ADMIN_EMAIL: process.env.ADMIN_EMAIL
-      },
-      env_production: {
         NODE_ENV: 'production',
         PORT: process.env.PORT,
         NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
@@ -29,8 +21,10 @@ module.exports = {
       ref: 'origin/main',
       repo: 'https://github.com/SrJuggernaut/srjuggernaut_dev',
       path: process.env.DEPLOY_PATH,
-      'post-deploy': 'pm2 --silent startOrRestart ecosystem.config.js',
+      'post-deploy': 'pm2 --silent startOrRestart ecosystem.config.js && pm2 save',
       env: {
+        APP_NAME: process.env.APP_NAME,
+        PORT: process.env.PORT,
         NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
         NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
         APPWRITE_KEY_SECRET: process.env.APPWRITE_KEY_SECRET,
@@ -43,8 +37,9 @@ module.exports = {
       ref: 'origin/develop',
       repo: 'https://github.com/SrJuggernaut/srjuggernaut_dev',
       path: process.env.DEPLOY_PATH,
-      'post-deploy': 'pm2 --silent startOrRestart ecosystem.config.js',
+      'post-deploy': 'pm2 --silent startOrRestart ecosystem.config.js && pm2 save',
       env: {
+        APP_NAME: process.env.APP_NAME,
         PORT: process.env.PORT,
         NEXT_PUBLIC_APPWRITE_ENDPOINT: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
         NEXT_PUBLIC_APPWRITE_PROJECT_ID: process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID,
