6e9b5636c3708fdf5ed3751cf6206058b598b662
SrJuggernaut <jugger@srjuggernaut.dev>
2024-04-11 15:33:34 -0600

    feat: image remote patterns from env varaibles

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
+const imageRemotePatternsFromEnv = () => {
+  const remoteImagePatterns = process.env.IMAGES_DOMAIN_LIST
+  if (!remoteImagePatterns) return []
+  return remoteImagePatterns.split(',').map(pattern => {
+    return {
+      protocol: pattern.startsWith('http') ? 'http' : 'https',
+      hostname: pattern.replace(/^https?:\/\//, '')
+    }
+  })
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   compiler: {
     styledComponents: true
+  },
+  images: {
+    remotePatterns: imageRemotePatternsFromEnv()
   }
 }
 
